6ae534a35861e3230f5f679573cab0abc5c0ed88
LeanNSP <58130895+LeanNSP@users.noreply.github.com>
2021-03-18 20:39:23 +0200

    run server

--- back/api/server.js
+++ back/api/server.js
@@ -1,1 +1,14 @@
-"use strict";
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(3300, () => {
+  console.log('Server started on port: 3300');
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log('Server Error');
+  server.close(() => process.exit(1));
+});
