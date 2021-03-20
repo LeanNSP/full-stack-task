517e4b41094fde0cff4490580ab165b75ce64597
LeanNSP <58130895+LeanNSP@users.noreply.github.com>
2021-03-20 10:41:47 +0200

    adds switch routes

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Suspense } from 'react';
+import { Switch } from 'react-router-dom';
+
+import { PublicRoute, PrivateRoute } from './Routes';
+
+import routes from '../routes';
 
 const App = () => {
   return (
-    <>
-      <p>test</p>
-    </>
+    <Suspense>
+      <Switch>
+        {routes.map(route =>
+          route.private ? (
+            <PrivateRoute key={route.path} {...route} />
+          ) : (
+            <PublicRoute key={route.path} {...route} />
+          ),
+        )}
+      </Switch>
+    </Suspense>
   );
 };
 
