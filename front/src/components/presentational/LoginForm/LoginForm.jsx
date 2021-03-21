6a6458d2c0eed0e37e916f4286afcf2232e6457c
LeanNSP <58130895+LeanNSP@users.noreply.github.com>
2021-03-21 12:07:32 +0200

    adds login form

---
+++ front/src/components/presentational/LoginForm/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import style from './LoginForm.module.css';
+
+const LoginForm = () => {
+  const { register, handleSubmit, errors } = useForm(); // development mode: add const { watch } = useForm();
+  const onSubmit = data => console.log(data);
+
+  // development mode
+  // watch input value by passing the name of it
+  //   console.log(watch('email'));
+  //   console.log(watch('password'));
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
+      <label className={style.label}>
+        <span>Email*</span>
+
+        <input name="email" placeholder="Enter email" ref={register({ required: true })} />
+        {errors.firstName && 'First name is required'}
+      </label>
+      <label className={style.label}>
+        <span>Password*</span>
+
+        <input name="password" placeholder="Enter password" ref={register({ required: true })} />
+        {errors.lastName && 'Last name is required'}
+      </label>
+      <button className={style.btn} type="submit">
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from "./components/App";
+import App from './components/App';
+
+import './css/reset.css';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
