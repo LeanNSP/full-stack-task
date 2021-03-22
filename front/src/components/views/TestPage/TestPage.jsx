848e8aa3b65293bccf3864deb1c199685a21de2b
LeanNSP <58130895+LeanNSP@users.noreply.github.com>
2021-03-22 09:27:01 +0200

    adds logout

--- front/src/components/views/TestPage/TestPage.jsx
+++ front/src/components/views/TestPage/TestPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { authOperations } from '../../../redux/auth';
 
 import style from './TestPage.module.css';
 
 const TestPage = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className={style.wrapper}>
       <h1 className={style.title}>Test Page</h1>
-      <button className={style.btn}>Logout</button>
+      <button className={style.btn} type="button" onClick={() => authOperations.logout(dispatch)}>
+        Logout
+      </button>
     </div>
   );
 };
--- front/src/services/fetchServer.js
+++ front/src/services/fetchServer.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3300';
 
-const fetchServer = {
-  tokenToHeader: {
-    set(token) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    },
-    unset() {
-      axios.defaults.headers.common['Authorization'] = '';
-    },
+export const tokenToHeader = {
+  set(token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   },
+  unset() {
+    axios.defaults.headers.common['Authorization'] = '';
+  },
+};
 
+const fetchServer = {
   post: async (url, item = null) => {
     try {
       const { data } = await axios.post(url, item);
